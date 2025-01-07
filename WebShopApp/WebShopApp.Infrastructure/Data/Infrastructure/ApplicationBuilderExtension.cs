16c22a5c804c86d02ca97a4c1b4eb05decefeb02
nkovacheva <n.kovacheva2020@pmg-pernik.com>
2025-01-07 11:42:06 +0200

    3

---
+++ WebShopApp/WebShopApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopApp.Infrastructure.Data.Infrastructure
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static async Task<ApplicationBuilder> PrepareDataBase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var services = serviceScope.ServiceProvider;
+            await RoleSeeder(services);
+            await SeedAdministrator(services);
+
+            return app;
+        }
+
+        private static async Task RoleSeeder(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Administrator", "Client" };
+            IdentityResult roleResult;
+
+            foreach (var role in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        private static async Task SeedAdministrator(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            if (await userManager.FindByNameAsync("admin") == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.FirstName = "admin";
+                user.LastName = "admin";
+                user.UserName = "admin";
+                user.Email = "admin@admin.com";
+                user.Address = "admin address";
+                user.PhoneNumber = "0888888888";
+                var result = await userManager.CreateAsync
+                (user, "Admin123456");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+    }
+}
